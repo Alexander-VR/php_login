77b92cc0081bfbe8c6fc236a53395cfc25180ff7
Alexander Velasquez <ale.vrs@outlook.com>
2021-11-05 16:04:57 -0500

    Agregamos algo al index.php y algunos estilos

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to your App</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h1>Please Login or SignUp</h1>
 
-    echo "works";
-
-?>
+    <a href="login.php">Login</a> or
+    <a href="signup.php">Signup</a>
+</body>
+</html>
